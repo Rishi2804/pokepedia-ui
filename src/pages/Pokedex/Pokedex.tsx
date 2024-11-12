f287ee544b52c2fd3a738d4d4b9b9ab11de6f957
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-11-12 12:30:15 -0500

    feat: header added

--- src/pages/Pokedex/Pokedex.tsx
+++ src/pages/Pokedex/Pokedex.tsx
@@ -1,32 +1,29 @@
 import {useParams} from "react-router-dom";
 import {urlToVersion} from "./utils.ts";
-import {PokedexRegion} from "../../global/enums.ts";
+import {PokedexRegion, VersionGroup} from "../../global/enums.ts";
 import {VersionToRegion} from "../../global/utils.ts";
-
+import Header from "./Header/Header.tsx";
 
 const Pokedex = () => {
     const { versionGroup: group } =  useParams<{ versionGroup: string }>()
 
     let dexes: PokedexRegion[] = [PokedexRegion.NATIONAL];
+    let version: VersionGroup | undefined = undefined;
 
     // Check if group is valid and exists in urlToVersion
     if (group && group in urlToVersion) {
         // Get the corresponding VersionGroup enum from the urlToVersion mapping
-        const versionKey = urlToVersion[group];
+        version = urlToVersion[group];
+
 
         // Use VersionGroup to fetch the associated PokedexRegion array from VersionToRegion
-        dexes = VersionToRegion[versionKey];
+        dexes = VersionToRegion[version];
     }
 
     return (
         <>
-            {/*<Header />*/}
+            <Header group={version} />
             {/*<PokemonList />*/}
-            <ul>
-                {dexes.map((region, index) => (
-                    <li key={index}>{region}</li>
-                ))}
-            </ul>
         </>
     )
 }
---
+++ src/pages/Pokedex/PokemonList/PokemonList.tsx
@@ -0,0 +1,14 @@
+import {useEffect} from "react";
+
+const PokemonList = () => {
+    useEffect(() => {
+
+    }, [])
+
+
+    return (
+        <></>
+    )
+}
+
+export default PokemonList
