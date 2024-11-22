f0c7194e8b9cb3cbef096f183bd11f54e5c546ea
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-11-21 22:53:59 -0500

    feat: Finalized tab styling

--- src/pages/Pokemon/FormTabs/styles.tsx
+++ src/pages/Pokemon/FormTabs/styles.tsx
@@ -1,11 +1,12 @@
 import {styled} from "@mui/system";
-import { Tabs, Tab } from "@mui/material";
+import {Tabs, Tab, alpha} from "@mui/material";
 
 export const StyledTabs = styled(Tabs)(({ theme }) => ({
-    borderBottom: `1px solid ${theme.palette.primaryBorder}`,
+    backgroundColor: theme.palette.background.default,
+    display: "inline-flex",
+    borderRadius: 15,
+    padding: theme.spacing(1),
     '& .MuiTabs-indicator': {
-        display: 'flex',
-        justifyContent: 'center',
         backgroundColor: 'transparent',
     },
 }));
@@ -18,17 +19,17 @@ export const StyledTab = styled((props: StyledTabProps) => (
     <Tab disableRipple {...props} />
 ))(({ theme }) => ({
     marginRight: theme.spacing(1),
-    backgroundColor: 'black',
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-    border: `1px solid ${theme.palette.primaryBorder}`,
-    color: 'white',
+    backgroundColor: theme.palette.background.default,
+    borderRadius: 12,
+    color: theme.palette.text.primary,
     '&.Mui-selected': {
         backgroundColor: theme.palette.background.paper,
-        color: 'white',
-        borderBottom: theme.palette.background.paper
+        color: theme.palette.text.primary,
+        border: `1px solid ${theme.palette.primaryBorder}`,
     },
     '&:hover': {
-        color: theme.palette.background.selected
+        backgroundColor: alpha(theme.palette.background.paper, 0.8),
+        color: theme.palette.background.selected,
+
     },
 }));
