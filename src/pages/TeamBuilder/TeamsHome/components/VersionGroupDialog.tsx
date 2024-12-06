125469a4896bf19eff87aa4162270c4641cf44bd
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2024-12-06 11:37:10 -0500

    feat: setup new team navigation

--- src/pages/TeamBuilder/TeamsHome/components/VersionGroupDialog.tsx
+++ src/pages/TeamBuilder/TeamsHome/components/VersionGroupDialog.tsx
@@ -2,17 +2,29 @@ import {Box, Button, Dialog, Divider, Stack} from "@mui/material";
 import {useState} from "react";
 import {VersionGroup} from "../../../../global/enums.ts";
 import {styled} from "@mui/system";
+import {useNavigate} from "react-router-dom";
 
 
 const VersionGroupDialog = () => {
     const [open, setOpen] = useState<boolean>(false)
-
+    const navigate = useNavigate();
     const handleClickOpen = () => {
         setOpen(true);
     };
 
     const handleClose = () => {
         setOpen(false);
+    }
+
+    const handleNavigate = (versionGroup: string) => {
+        if (versionGroup === "Home") {
+            navigate(`/team-builder/new/national`)
+        } else if (versionGroup === "Brilliant Diamond/Shining Pearl") {
+            navigate(`/team-builder/new/brilliant-diamond-and-shining-pearl`)
+        } else {
+            const string = versionGroup.toLowerCase().replace(/[ /]/g, "-")
+            navigate(`/team-builder/new/${string}`);
+        }
     }
 
     const VersionGroupText = styled(Box)(({ theme }) => ({
@@ -32,10 +44,10 @@ const VersionGroupDialog = () => {
             <Button variant="contained" sx={{padding: 1.5}} onClick={handleClickOpen}>Create New Team</Button>
             <Dialog open={open} onClose={handleClose} PaperProps={{sx: {width: '30rem'}}}>
                 <Stack divider={<Divider flexItem /> }>
-                    <VersionGroupText>Home</VersionGroupText>
+                    <VersionGroupText onClick={() => handleNavigate('Home')}>Home</VersionGroupText>
                     {
                          Object.values(VersionGroup).reverse().map(group => (
-                             <VersionGroupText>{group}</VersionGroupText>
+                             <VersionGroupText onClick={() => handleNavigate(group)}>{group}</VersionGroupText>
                          ))
                     }
                 </Stack>
