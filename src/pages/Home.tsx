8ae9dfeccc941b3be10e367e95f5eada40a3196d
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2025-01-07 21:16:31 -0500

    feat: updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,30 @@
-import PokemonImg from "../components/PokemonImg/PokemonImg.tsx";
-import TypeIcon from "../components/TypeIcon/TypeIcon.tsx";
-import {PokemonType} from "../global/enums.ts";
+import {Stack, Typography} from "@mui/material";
+import MetaData from "../components/MetaData/MetaData.tsx";
 
 export const Home = () => {
     return (
         <>
-            <h1>Home Page</h1>
-            <PokemonImg id={448} />
-            <TypeIcon type={PokemonType.ELECTRIC} variant={"outlined"} />
+            <MetaData pageTitle={"PokePedia"} />
+            <Typography variant="h1" sx={{textAlign: 'center', my: 2}}>Welcome to PokePedia!</Typography>
+            <Stack spacing={1}>
+            <Typography>Welcome, Trainer! 🌟</Typography>
+            <Typography>
+                You've just arrived at the most comprehensive and up-to-date resource for everything Pokémon.
+                Whether you're beginning your journey or striving to become a true Pokémon Master,
+                this is the place to be!
+            </Typography>
+            <Typography>
+                Explore every Pokémon, from their types and evolutions to their unique moves and abilities.
+                Discover in-depth stats, battle strategies, and some fun facts you may not have known,
+                including the latest from Pokémon Scarlet and Violet.
+            </Typography>
+            <Typography>
+                Our encyclopedia is your go-to guide for everything you need to know to master the Pokémon universe.
+            </Typography>
+            <Typography>
+                Get ready to catch ‘em all—knowledge and Pokémon alike!
+            </Typography>
+            </Stack>
         </>
-
-
     )
 }
