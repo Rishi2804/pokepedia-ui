b3020900af2207df0bbf76b0fbac91fb23805819
Rishi2804 <77213736+Rishi2804@users.noreply.github.com>
2026-03-01 17:35:27 -0500

    feat: removed local pokemon images and pulled from minio object storage

--- src/components/PokemonImg/PokemonImg.tsx
+++ src/components/PokemonImg/PokemonImg.tsx
@@ -1,5 +1,5 @@
-import {FC, useEffect, useState} from "react";
-import {femaleImgs, specialImgs} from "./constants.ts";
+import {FC} from "react";
+import {specialImgs} from "./constants.ts";
 
 interface IPokemonImgProps {
     id: number
@@ -7,38 +7,28 @@ interface IPokemonImgProps {
     female?: boolean
 }
 
+const BASE_URL: string = `${import.meta.env.VITE_POKEMON_IMAGES_URL}`;
+
 const PokemonImg: FC<IPokemonImgProps> = ({id, shiny, female}) => {
-    const [homeImg, setHomeImg] = useState<string>("")
-    const [officialImg, setOfficialImg] = useState<string>("")
-
-    useEffect(() => {
-        let homeImgBuild: string = `./assets/home`
-        let artworkImg: string = `./assets/official`
-        if (shiny) {
-            homeImgBuild += "/shiny"
-            artworkImg += "/shiny"
-        }
-        if (female && femaleImgs.includes(id)) {
-            homeImgBuild += "/female"
-            artworkImg += "/female"
-        }
-        homeImgBuild += `/${id}.png`
-        artworkImg += `/${id}.png`
-        setHomeImg(homeImgBuild)
-        setOfficialImg(artworkImg)
-
-    }, [id, shiny, female])
-
-    function getImg() {
-        if (specialImgs.includes(id)) {
-            return new URL(officialImg, import.meta.url).href
-        }
-
-        return new URL(homeImg, import.meta.url).href
+    let folder: string = "home";
+    if (specialImgs.includes(id)) {
+        folder = "official";
     }
 
+    let path = `${BASE_URL}/${folder}`
+
+    if (shiny) {
+        path += "/shiny"
+    }
+
+    if (female) {
+        path += "/female"
+    }
+
+    const imgURL: string = `${path}/${id}.png`
+
     return (
-        <img src={getImg()} alt={`${id}`}/>
+        <img src={imgURL} alt={`${id}`}/>
     )
 }
 
